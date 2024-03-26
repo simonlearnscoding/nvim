d68c254bea03e2f72b4fc4c4f59b68c34eea83ce
simonlearnscoding <40177876+simonlearnscoding@users.noreply.github.com>
2024-03-26 14:06:46 +0100

    add telescope icons

--- lua/simon/core/telescope-mappings.lua
+++ lua/simon/core/telescope-mappings.lua
@@ -10,35 +10,35 @@ local maps = { i = {}, n = {}, v = {}, t = {} }
 --   desc = 'Git commits',
 -- }
 local prefix = '<leader>f'
-maps.n[prefix .. 'f'] = { desc = '󰍉 Find' }
-maps.n[prefix .. 'n'] = { '<cmd>Telekasten find_notes<cr>', desc = 'Find Notes' }
-maps.n[prefix .. 't'] = { '<cmd>TodoTelescope<CR>', desc = 'TODO' }
-maps.n[prefix .. 'g'] = { desc = ' git...' }
-maps.n[prefix .. 'r'] = { desc = ' reset...' }
-maps.n[prefix .. 'gb'] = { '<cmd>Telescope git_branches<CR>', desc = ' ...branches' }
-maps.n[prefix .. 'gc'] = { '<cmd>Telescope git_commits<CR>', desc = ' ...commits' }
-maps.n[prefix .. 'gs'] = { '<cmd>Telescope git_status<CR>', desc = ' ...status' }
-maps.n[prefix .. 'gf'] = { '<cmd>Telescope git_files<CR>', desc = ' ...files' }
-maps.n[prefix .. 'd'] = { "<cmd>lua require('telescope.builtin').diagnostics()<CR>", desc = 'Diagnostics' }
-maps.n[prefix .. 'l'] = { "<cmd>lua require('telescope.builtin').lsp_references()<CR>", desc = 'LSP References' }
-maps.n[prefix .. 'w'] = { "<cmd>lua require('telescope.builtin').live_grep()<CR>", desc = 'find word' }
-maps.n[prefix .. 'r'] = { "<cmd>lua require('telescope.builtin').lsp_references()<CR>", desc = 'LSP References' }
+maps.n['<leader>f'] = { desc = '󰍉 Find..' }
+maps.n[prefix .. 'n'] = { '<cmd>Telekasten find_notes<cr>', desc = ' [n]otes' }
+maps.n[prefix .. 't'] = { '<cmd>TodoTelescope<CR>', desc = '󰱑 [t]asks' }
+maps.n[prefix .. 'g'] = { desc = ' [g]it..' }
+maps.n[prefix .. 'r'] = { desc = ' [r]eset..' }
+maps.n[prefix .. 'gb'] = { '<cmd>Telescope git_branches<CR>', desc = ' [b]ranches' }
+maps.n[prefix .. 'gc'] = { '<cmd>Telescope git_commits<CR>', desc = ' [c]ommits' }
+maps.n[prefix .. 'gs'] = { '<cmd>Telescope git_status<CR>', desc = ' [s]tatus' }
+maps.n[prefix .. 'gf'] = { '<cmd>Telescope git_files<CR>', desc = ' [f]iles' }
+maps.n[prefix .. 'd'] = { "<cmd>lua require('telescope.builtin').diagnostics()<CR>", desc = '  [d]iagnostics' }
+maps.n[prefix .. 'l'] = { "<cmd>lua require('telescope.builtin').lsp_references()<CR>", desc = ' [l]sp' }
+maps.n[prefix .. 'w'] = { "<cmd>lua require('telescope.builtin').live_grep()<CR>", desc = ' [w]ord' }
+-- maps.n[prefix .. 'r'] = { "<cmd>lua require('telescope.builtin').lsp_references()<CR>", desc = 'LSP References' }
 
 maps.n[prefix .. 'o'] = {
   function()
     require('telescope.builtin').oldfiles()
   end,
-  desc = 'Find file history',
+  desc = ' [o]ld files',
 }
 
 maps.n[prefix .. 'f'] = {
   function()
     require('telescope.builtin').find_files()
   end,
-  desc = 'Find files',
+  desc = ' [f]iles',
 }
 
-maps.n[prefix .. 'C'] = {
+maps.n[prefix .. 'r'] = {
   function()
     local cwd = vim.fn.expand '~/react-components' -- use the home directory as the working directory
     require('telescope.builtin').find_files {
@@ -47,10 +47,10 @@ maps.n[prefix .. 'C'] = {
       no_ignore = true,
     }
   end,
-  desc = 'Find react components',
+  desc = ' [r]eact',
 }
 
-maps.n[prefix .. 'F'] = {
+maps.n[prefix .. 'a'] = {
   function()
     local cwd = vim.fn.expand '~' -- use the home directory as the working directory
     require('telescope.builtin').find_files {
@@ -59,15 +59,15 @@ maps.n[prefix .. 'F'] = {
       no_ignore = true,
     }
   end,
-  desc = 'Find all files',
+  desc = ' [a]ll files',
 }
 
-maps.n[prefix .. 'z'] = { '<cmd>Telescope zoxide list<cr>', desc = 'Zoxide' }
-maps.n[prefix .. 'B'] = { '<cmd>Telescope bibtex<cr>', desc = 'Find BibTeX' }
+maps.n[prefix .. 'z'] = { '<cmd>Telescope zoxide list<cr>', desc = ' [z]oxide' }
+maps.n[prefix .. 'b'] = { '<cmd>Telescope bibtex<cr>', desc = ' [b]ibliography' }
 maps.n[prefix .. 'p'] = {
   function()
     require('telescope').extensions.projects.projects {}
   end,
-  desc = 'Find projects',
+  desc = ' [p]rojects',
 }
 utils.set_mappings(maps)
