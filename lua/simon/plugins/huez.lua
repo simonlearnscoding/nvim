b0bc2c52b32f846a98e1aaabb4c897599b0a753f
Simon M <simon.muscas@gmail.com>
2024-06-23 13:47:45 +0200

    huez

--- lua/simon/plugins/huez.lua
+++ lua/simon/plugins/huez.lua
@@ -1,18 +1,10 @@
--- Lazy
-
 return {
-  event = 'BufWinEnter',
-  'vague2k/huez.nvim',
-  dependencies = {
-    -- You probably already have this installed, highly reccomended you do.
-    'nvim-telescope/telescope.nvim',
-    -- If using vim.ui, this plugin will give you a better experience
-    'stevearc/dressing.nvim',
-  },
+  "vague2k/huez.nvim",
+  branch = "stable",
+  event = "UIEnter",
   config = function()
-    require('huez').setup()
-    local colorscheme = require('huez.api').get_colorscheme()
-    vim.cmd('colorscheme ' .. colorscheme)
+    require("huez").setup({})
+
     vim.keymap.set('n', '<leader>co', '<cmd>Huez<CR>', {})
   end,
 }
