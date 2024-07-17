58ce09ad5325d3319f81108eb9a6b406d72a36e2
simonlearnscoding <40177876+simonlearnscoding@users.noreply.github.com>
2024-07-17 22:05:56 +0200

    breakpointstuff

--- lua/simon/plugins/autotag.lua
+++ lua/simon/plugins/autotag.lua
@@ -2,25 +2,22 @@
 return {
   event = 'VeryLazy',
   'windwp/nvim-ts-autotag',
-  dependendencies = {
-    'nvim-treesitter/nvim-treesitter'
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
   },
   config = function()
-    require('nvim-ts-autotag').setup({
+    require('nvim-ts-autotag').setup {
       enable_close_on_slash = false,
-    })
+    }
 
-    vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics,
-      {
-        underline = true,
-        virtual_text = {
-          spacing = 5,
-          severity_limit = 'Warning',
-        },
-        update_in_insert = true,
-      }
-    )
+    vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+      underline = true,
+      virtual_text = {
+        spacing = 5,
+        severity = { min = vim.diagnostic.severity.WARN },
+      },
+      update_in_insert = true,
+    })
 
     -- require('nvim-treesitter.configs').setup({
     --   autotag = {
--- lua/simon/plugins/vim-transparent.lua
+++ lua/simon/plugins/vim-transparent.lua
@@ -2,8 +2,11 @@ if vim.g.neovide then
   return {}
 end
 return {
-  "tribela/vim-transparent",
+  'tribela/vim-transparent',
   config = function()
+    if vim.g.neovide then
+      vim.g.transparent_enabled = 0
+    end
     vim.g.transparent_enabled = 1
     -- vim.g.transparent_darkness = 0.7
   end,
