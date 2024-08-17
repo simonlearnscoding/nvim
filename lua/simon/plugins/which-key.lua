9549fd778fb3f3020e8ac334b98c8ff2d33b1431
simonlearnscoding <40177876+simonlearnscoding@users.noreply.github.com>
2024-08-17 09:57:29 +0200

    which key and hover

--- lua/simon/plugins/which-key.lua
+++ lua/simon/plugins/which-key.lua
@@ -1,32 +1,30 @@
-return
-
+return 
 {
   'folke/which-key.nvim',
-  event = "VeryLazy",
+  event = 'VeryLazy',
   config = function()
     vim.o.timeoutlen = 500
-    local wk = require("which-key")
-    wk.register({
-      ['<leader>'] =
-      {
+    local wk = require 'which-key'
+    wk.register {
+      ['<leader>'] = {
         d = {
-          name = 'diagnostics'
+          name = 'diagnostics',
         },
 
         C = {
-          name = "Color Schemes", -- Group name
-          g = "Gruvbox",
-          k = "Kanagawa",
-          r = "Rose Pine",
-          c = "Catppuccin",
-          m = "Miramare",
-          n = "Nightfox",
-          G = "Gruvbox Material",
-          s = "Sonokai",
-          t = "Tokyonight",
-          e = "Everforest",
+          name = 'Color Schemes', -- Group name
+          g = 'Gruvbox',
+          k = 'Kanagawa',
+          r = 'Rose Pine',
+          c = 'Catppuccin',
+          m = 'Miramare',
+          n = 'Nightfox',
+          G = 'Gruvbox Material',
+          s = 'Sonokai',
+          t = 'Tokyonight',
+          e = 'Everforest',
         },
-      }
-    })
-  end
+      },
+    }
+  end,
 }
