b70de3c078c64c2028602d618c3d725e2a4f89bd
simonlearnscoding <40177876+simonlearnscoding@users.noreply.github.com>
2024-10-14 11:00:15 +0200

    update lualine

--- lua/simon/plugins/oldworld-and-lualine.lua
+++ lua/simon/plugins/oldworld-and-lualine.lua
@@ -1,26 +1,47 @@
 return {
-
-  "dgox16/oldworld.nvim",
+  'dgox16/oldworld.nvim',
   dependencies = {
     'nvim-lualine/lualine.nvim',
   },
   lazy = false,
   config = function()
-    require("oldworld").setup({
-    })
-    local colors = require("oldworld.palette")
+    require('oldworld').setup {}
+    local colors = require 'oldworld.palette'
 
+    -- Mode icons definition
+    local mode_icons = {
+      n = '(ಠ_ಠ)', -- Normal
+      i = '(◠‿◠)', -- Insert
+      v = '(ʘ‿ʘ)', -- Visual
+      V = '(ʘ‿ʘ)', -- Visual Line
+      [''] = '(ʘ‿ʘ)', -- Visual Block
+      c = '(°ʖ°)', -- Command
+      no = '', -- Operator Pending
+      s = '(¬‿¬)', -- Select
+      S = '', -- Select Line
+      [''] = '', -- Select Block
+      ic = '', -- Insert completion
+      R = '律', -- Replace
+      Rv = '律', -- Virtual Replace
+      cv = '', -- Command-line editing
+      ce = '', -- Normal Ex mode
+      r = '', -- Prompt (hit-enter prompt)
+      rm = '', -- More prompt
+      ['r?'] = '', -- Confirm
+      ['!'] = 'ﲵ', -- Shell/terminal
+      t = '', -- Terminal mode
+    }
     local modecolor = {
       n = colors.red,
       i = colors.cyan,
       v = colors.purple,
-      [""] = colors.purple,
+      [''] = colors.purple,
       V = colors.red,
       c = colors.yellow,
       no = colors.red,
       s = colors.yellow,
       S = colors.yellow,
-      [""] = colors.yellow,
+      [''] = colors.yellow,
       ic = colors.yellow,
       R = colors.green,
       Rv = colors.purple,
@@ -28,8 +49,8 @@ return {
       ce = colors.red,
       r = colors.cyan,
       rm = colors.cyan,
-      ["r?"] = colors.cyan,
-      ["!"] = colors.red,
+      ['r?'] = colors.cyan,
+      ['!'] = colors.red,
       t = colors.bright_red,
     }
 
@@ -37,8 +58,8 @@ return {
       normal = {
         a = { fg = colors.bg_dark, bg = colors.blue },
         b = { fg = colors.blue, bg = colors.white },
-        c = { fg = colors.white, bg = colors.bg_dark },
-        z = { fg = colors.white, bg = colors.bg_dark },
+        c = { fg = colors.white, bg = 'none' },
+        z = { fg = colors.white, bg = 'none' },
       },
       insert = { a = { fg = colors.bg_dark, bg = colors.orange } },
       visual = { a = { fg = colors.bg_dark, bg = colors.green } },
@@ -47,42 +68,42 @@ return {
 
     local space = {
       function()
-        return " "
+        return ' '
       end,
-      color = { bg = colors.bg_dark, fg = colors.blue },
+      color = { bg = 'none', fg = colors.blue },
     }
 
     local filename = {
-      "filename",
-      color = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-      separator = { left = "", right = "" },
+      'filename',
+      color = { bg = colors.blue, fg = colors.bg, gui = 'bold' },
+      separator = { left = '', right = '' },
     }
 
     local filetype = {
-      "filetype",
+      'filetype',
       icons_enabled = true,
-      color = { bg = colors.gray2, fg = colors.blue, gui = "italic,bold" },
-      separator = { left = "", right = "" },
+      color = { bg = colors.gray2, fg = colors.blue, gui = 'italic,bold' },
+      separator = { left = '', right = '' },
     }
 
     local branch = {
-      "branch",
-      icon = "",
-      color = { bg = colors.green, fg = colors.bg, gui = "bold" },
-      separator = { left = "", right = "" },
+      'branch',
+      icon = '',
+      color = { bg = colors.green, fg = colors.bg, gui = 'bold' },
+      separator = { left = '', right = '' },
     }
 
     local location = {
-      "location",
-      color = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
-      separator = { left = "", right = "" },
+      'location',
+      color = { bg = colors.yellow, fg = colors.bg, gui = 'bold' },
+      separator = { left = '', right = '' },
     }
 
     local diff = {
-      "diff",
-      color = { bg = colors.gray2, fg = colors.bg, gui = "bold" },
-      separator = { left = "", right = "" },
-      symbols = { added = " ", modified = " ", removed = " " },
+      'diff',
+      color = { bg = 'none', fg = colors.bg, gui = 'bold' },
+      separator = { left = '', right = '' },
+      symbols = { added = ' ', modified = ' ', removed = ' ' },
 
       diff_color = {
         added = { fg = colors.green },
@@ -91,39 +112,41 @@ return {
       },
     }
 
+    -- Customize the mode display with icons
     local modes = {
-      "mode",
-      color = function()
-        local mode_color = modecolor
-        return { bg = mode_color[vim.fn.mode()], fg = colors.bg_dark, gui = "bold" }
+      function()
+        local mode = vim.fn.mode()
+        return mode_icons[mode] or mode -- Show icon or fallback to mode text
       end,
-      separator = { left = "", right = "" },
+      color = function()
+        return { bg = modecolor[vim.fn.mode()], fg = colors.bg_dark, gui = 'bold' }
+      end,
+      separator = { left = '', right = '' },
     }
-
     local function getLspName()
       local buf_clients = vim.lsp.buf_get_clients()
       local buf_ft = vim.bo.filetype
       if next(buf_clients) == nil then
-        return "  No servers"
+        return '  No servers'
       end
       local buf_client_names = {}
 
       for _, client in pairs(buf_clients) do
-        if client.name ~= "null-ls" then
+        if client.name ~= 'null-ls' then
           table.insert(buf_client_names, client.name)
         end
       end
 
-      local lint_s, lint = pcall(require, "lint")
+      local lint_s, lint = pcall(require, 'lint')
       if lint_s then
         for ft_k, ft_v in pairs(lint.linters_by_ft) do
-          if type(ft_v) == "table" then
+          if type(ft_v) == 'table' then
             for _, linter in ipairs(ft_v) do
               if buf_ft == ft_k then
                 table.insert(buf_client_names, linter)
               end
             end
-          elseif type(ft_v) == "string" then
+          elseif type(ft_v) == 'string' then
             if buf_ft == ft_k then
               table.insert(buf_client_names, ft_v)
             end
@@ -131,10 +154,10 @@ return {
         end
       end
 
-      local ok, conform = pcall(require, "conform")
-      local formatters = table.concat(conform.list_formatters_for_buffer(), " ")
+      local ok, conform = pcall(require, 'conform')
+      local formatters = table.concat(conform.list_formatters_for_buffer(), ' ')
       if ok then
-        for formatter in formatters:gmatch("%w+") do
+        for formatter in formatters:gmatch '%w+' do
           if formatter then
             table.insert(buf_client_names, formatter)
           end
@@ -150,9 +173,9 @@ return {
           hash[v] = true
         end
       end
-      local language_servers = table.concat(unique_client_names, ", ")
+      local language_servers = table.concat(unique_client_names, ', ')
 
-      return "  " .. language_servers
+      return '  ' .. language_servers
     end
 
     -- local macro = {
@@ -162,33 +185,33 @@ return {
     -- }
 
     local dia = {
-      "diagnostics",
-      sources = { "nvim_diagnostic" },
-      symbols = { error = " ", warn = " ", info = " ", hint = " " },
+      'diagnostics',
+      sources = { 'nvim_diagnostic' },
+      symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' },
       diagnostics_color = {
         error = { fg = colors.red },
         warn = { fg = colors.yellow },
         info = { fg = colors.purple },
         hint = { fg = colors.cyan },
       },
-      color = { bg = colors.gray2, fg = colors.blue, gui = "bold" },
-      separator = { left = "" },
+      color = { bg = colors.gray2, fg = colors.blue, gui = 'bold' },
+      separator = { left = '' },
     }
 
     local lsp = {
       function()
         return getLspName()
       end,
-      separator = { left = "", right = "" },
-      color = { bg = colors.purple, fg = colors.bg, gui = "italic,bold" },
+      separator = { left = '', right = '' },
+      color = { bg = colors.purple, fg = colors.bg, gui = 'italic,bold' },
     }
 
-    require("lualine").setup({
+    require('lualine').setup {
       options = {
         icons_enabled = true,
         theme = theme,
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        component_separators = { left = '', right = '', bg = 'none' },
+        section_separators = { left = '', right = '', bg = 'none' },
         disabled_filetypes = {
           statusline = {},
           winbar = {},
@@ -200,9 +223,10 @@ return {
 
       sections = {
         lualine_a = {
-          modes,
+          space,
         },
         lualine_b = {
+          modes,
           space,
         },
         lualine_c = {
@@ -212,13 +236,14 @@ return {
           branch,
           diff,
           space,
-          location,
         },
         lualine_x = {
           space,
         },
         lualine_y = { space },
         lualine_z = {
+          location,
+          space,
           dia,
           lsp,
         },
@@ -226,12 +251,11 @@ return {
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
         lualine_y = {},
         lualine_z = {},
       },
-    })
+    }
   end,
-
 }
