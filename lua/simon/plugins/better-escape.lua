c41c5cb3a756bda01894ce792dd3bfeafbb97299
Simon M <simon.muscas@gmail.com>
2024-10-26 15:53:08 +0200

    better escape new setup

--- lua/simon/plugins/better-escape.lua
+++ lua/simon/plugins/better-escape.lua
@@ -2,9 +2,40 @@ return {
   event = 'BufEnter',
   'max397574/better-escape.nvim',
   config = function()
+    -- lua, default settings
     require('better_escape').setup {
-      mapping = { 'jk', 'jj' }, -- a table with mappings to use
-      timeout = 200,            -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+      timeout = vim.o.timeoutlen,
+      default_mappings = true,
+      mappings = {
+        i = {
+          j = {
+            -- These can all also be functions
+            k = '<Esc>',
+            j = '<Esc>',
+          },
+        },
+        c = {
+          j = {
+            k = '<Esc>',
+            j = '<Esc>',
+          },
+        },
+        t = {
+          j = {
+            k = '<C-\\><C-n>',
+          },
+        },
+        v = {
+          j = {
+            k = '<Esc>',
+          },
+        },
+        s = {
+          j = {
+            k = '<Esc>',
+          },
+        },
+      },
     }
   end,
 }
