54305d6b024cff5991aad6ab14546ca95e8e679f
Simon M <simon.muscas@gmail.com>
2024-10-26 16:26:55 +0200

    comment ts

--- lua/simon/plugins/comment-nvim.lua
+++ lua/simon/plugins/comment-nvim.lua
@@ -2,6 +2,12 @@
 return {
   {
     'JoosepAlviste/nvim-ts-context-commentstring',
+
+    config = function()
+      require('ts_context_commentstring').setup {
+        enable_autocmd = false,
+      }
+    end,
   },
   {
     'terrortylor/nvim-comment',
