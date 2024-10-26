4ac44832c2c1348a175524d9516af84235c347ec
Simon M <simon.muscas@gmail.com>
2024-10-26 11:06:08 +0200

    autoload dir session

--- lua/simon/plugins/session-manager.lua
+++ lua/simon/plugins/session-manager.lua
@@ -2,11 +2,11 @@ return {
   commit = 'b552ee8', -- Pinning to the specific commit
   'Shatur/neovim-session-manager',
   config = function()
-    local session_manager = require('session_manager')
-    local config = require('session_manager.config')
+    local session_manager = require 'session_manager'
+    local config = require 'session_manager.config'
 
     session_manager.setup {
-      autoload_mode = config.AutoloadMode.Disabled, -- Disabled, CurrentDir, LastSession
+      autoload_mode = config.AutoloadMode.CurrentDir, -- Disabled, CurrentDir, LastSession
     }
 
     -- Safely set up key mappings to avoid conflicts
@@ -14,11 +14,11 @@ return {
     local function safe_load_session()
       local status_ok, err = pcall(session_manager.load_session)
       if not status_ok then
-        vim.notify("Failed to load session: " .. err, vim.log.levels.ERROR)
+        vim.notify('Failed to load session: ' .. err, vim.log.levels.ERROR)
       end
     end
 
     -- Set keymap with safety check
-    set_keymap('n', '<leader>s', safe_load_session, { desc = "Load Session" })
-  end
+    set_keymap('n', '<leader>s', safe_load_session, { desc = 'Load Session' })
+  end,
 }
